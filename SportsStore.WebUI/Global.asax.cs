b8ae31eb91d4023a7f6804c2d0deab022b861a19
unknown <oozturkplus@gmail.com>
2014-09-30 21:55:28 +0300

    Deneme

--- SportsStore.WebUI/Global.asax.cs
+++ SportsStore.WebUI/Global.asax.cs
@@ -17,6 +17,8 @@ namespace SportsStore.WebUI
         {
             AreaRegistration.RegisterAllAreas();
 
+
+
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
