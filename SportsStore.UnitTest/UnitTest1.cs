8c57de1f738f190a0b49bd8b612d8e8133551d4a
oozturk <oozturkplus@gmail.com>
2014-10-08 17:43:47 +0300

    Link Unit Test

--- SportsStore.UnitTest/UnitTest1.cs
+++ SportsStore.UnitTest/UnitTest1.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using SportsStore.WebUI.Models;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
 using SportsStore.WebUI.Controllers;
 using System.Collections.Generic;
+using SportsStore.WebUI.HtmlHelpers;
 using System.Linq;
+using System.Web.Mvc;
 
 namespace SportsStore.UnitTest
 {
@@ -15,7 +18,7 @@ namespace SportsStore.UnitTest
         [TestMethod]
         public void Can_Paginate()
         {
-            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            /*Mock<IProductRepository> mock = new Mock<IProductRepository>();
             mock.Setup(m => m.Products).Returns(
                 new Product[]{
                     new Product {ProductID=1,Name="P1"},
@@ -36,7 +39,29 @@ namespace SportsStore.UnitTest
             Product[] prodArray = result.ToArray();
             Assert.IsTrue(prodArray.Length == 2);
             Assert.AreEqual(prodArray[0].Name, "P4");
-            Assert.AreEqual(prodArray[1].Name, "P5");
+            Assert.AreEqual(prodArray[1].Name, "P5");*/
+        }
+
+        [TestMethod]
+        public void Can_Generate_Page_Links()
+        {
+
+
+            HtmlHelper myHelper = null;
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+
+            Func<int, string> pageUrlDelegate = i => "Page" + i;
+
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
+
+            Assert.AreEqual(result.ToString(), @"<a href=""Page1"">1</a>"
+                + @"<a class=""selected"" href=""Page2"">2</a>"
+                + @"<a href=""Page3"">3</a>");
         }
     }
 }
--- SportsStore.WebUI/HtmlHelpers/PagingHelpers.cs
+++ SportsStore.WebUI/HtmlHelpers/PagingHelpers.cs
@@ -10,18 +10,16 @@ namespace SportsStore.WebUI.HtmlHelpers
 {
     public static class PagingHelpers
     {
-        public static MvcHtmlString PageLinks(this HtmlHelper html, PagingInfo pagingInfo, Func<int, string> pageUrl)
+        public static MvcHtmlString PageLinks(this HtmlHelper html,PagingInfo pagingInfo,Func<int, string> pageUrl)
         {
             StringBuilder result = new StringBuilder();
-
             for (int i = 1; i <= pagingInfo.TotalPages; i++)
             {
-                TagBuilder tag = new TagBuilder("a");
+                TagBuilder tag = new TagBuilder("a"); // Construct an <a> tag
                 tag.MergeAttribute("href", pageUrl(i));
                 tag.InnerHtml = i.ToString();
                 if (i == pagingInfo.CurrentPage)
                     tag.AddCssClass("selected");
-
                 result.Append(tag.ToString());
             }
 
