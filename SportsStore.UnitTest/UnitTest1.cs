c3b8f2a762751f69b737ba94013dfccea8c88c64
unknown <oozturkplus@gmail.com>
2014-10-09 21:44:02 +0300

    Navigate Page 207

--- SportsStore.UnitTest/UnitTest1.cs
+++ SportsStore.UnitTest/UnitTest1.cs
@@ -31,7 +31,7 @@ namespace SportsStore.UnitTest
             ProductController controller = new ProductController(mock.Object);
             controller.PageSize = 3;
             // Act
-            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(null,2).Model;
             // Assert
             PagingInfo pageInfo = result.PagingInfo;
             Assert.AreEqual(pageInfo.CurrentPage, 2);
@@ -87,5 +87,30 @@ namespace SportsStore.UnitTest
                 + @"<a class=""selected"" href=""Page2"">2</a>"
                 + @"<a href=""Page3"">3</a>");
         }
+
+        [TestMethod]
+        public void Can_Filter_Products() {
+        // Arrange
+        // - create the mock repository
+        Mock<IProductRepository> mock = new Mock<IProductRepository>();
+        mock.Setup(m => m.Products).Returns(new Product[] {
+        new Product {ProductID = 1, Name = "P1", Category = "Cat1"},
+        new Product {ProductID = 2, Name = "P2", Category = "Cat2"},
+        new Product {ProductID = 3, Name = "P3", Category = "Cat1"},
+        new Product {ProductID = 4, Name = "P4", Category = "Cat2"},
+        new Product {ProductID = 5, Name = "P5", Category = "Cat3"}
+        }.AsQueryable());
+        // Arrange - create a controller and make the page size 3 items
+        ProductController controller = new ProductController(mock.Object);
+
+        controller.PageSize = 3;
+        // Action
+        Product[] result = ((ProductsListViewModel)controller.List("Cat2", 1).Model)
+        .Products.ToArray();
+        // Assert
+        Assert.AreEqual(result.Length, 2);
+        Assert.IsTrue(result[0].Name == "P2" && result[0].Category == "Cat2");
+        Assert.IsTrue(result[1].Name == "P4" && result[1].Category == "Cat2");
+}
     }
 }
