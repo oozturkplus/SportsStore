919faf5722c724b22a5885a46eb7d17fbd84e68e
unknown <oozturkplus@gmail.com>
2014-10-14 10:52:54 +0300

    Creating the Account Controller

--- SportsStore.WebUI/Controllers/AdminController.cs
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -8,6 +8,7 @@ using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Controllers
 {
+    [Authorize]
     public class AdminController : Controller
     {
         private IProductRepository repository;
@@ -43,6 +44,23 @@ namespace SportsStore.WebUI.Controllers
                 // there is something wrong with the data values
                 return View(product);
             }
+        }
+
+        public ViewResult Create()
+        {
+            return View("Edit", new Product());
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int productId)
+        {
+            Product deletedProduct = repository.DeleteProduct(productId);
+            if (deletedProduct != null)
+            {
+                TempData["message"] = string.Format("{0} was deleted",
+                deletedProduct.Name);
+            }
+            return RedirectToAction("Index");
         }
 
     }
